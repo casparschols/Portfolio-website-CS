85d8e9f3c411d09dbbb7853a51d12efcd3ba0e2a
casparschols <cs@casparschols.com>
2026-06-11 13:52:36 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -244,11 +244,12 @@ document.querySelectorAll('[data-slideshow]').forEach(function(slideshow) {
     e.preventDefault();
     e.stopPropagation();
 
- var isDesktopTopStrip =
+var isDesktopTopStrip =
   window.innerWidth > 1024 &&
   (
     slideshow.closest('.detail-gallery-top') ||
-    slideshow.closest('.de-vice-responsive-gallery')
+    slideshow.closest('.de-vice-responsive-gallery') ||
+    slideshow.closest('.product-gallery')
   );
 
     var imgToOpen;
